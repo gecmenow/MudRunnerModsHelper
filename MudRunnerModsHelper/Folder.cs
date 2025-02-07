cc9ebdd425c88f6c3036881300b31bda51664895
gecmenow <gector456@gmail.com>
2025-02-07 14:09:05 +0200

    init comit

---
+++ MudRunnerModsHelper/Folder.cs
@@ -0,0 +1,67 @@
+﻿namespace MudRunnerModsHelper;
+
+internal static class Folder
+{
+    private static List<(string SourcePath, string DestinationPath)> _movedFolders = [];
+    private static string TempPath = string.Empty;
+    private static string[] _subdirectories = [];
+
+    private const string tempFolder = "temp";
+
+    public static void CheckFolderToExists()
+    {
+        var mediaPath = File.ReadAllLines(Settings.Config).First() + @"\Media";
+        TempPath = Path.Combine(mediaPath, tempFolder);
+
+        if (!Directory.Exists(mediaPath))
+        {
+            Console.WriteLine("Media folder is missing.");
+            return;
+        }
+
+        // Get all subdirectories in the Media folder
+        _subdirectories = Directory.GetDirectories(mediaPath);
+
+        if (_subdirectories.Length is 0)
+        {
+            Console.WriteLine("Nothing to move. Exiting.");
+            return;
+        }
+    }
+
+    public static void MoveFilders()
+    {
+        Directory.CreateDirectory(TempPath);
+
+        // Move subdirectories into the "temp" folder
+        foreach (var subdir in _subdirectories)
+        {
+            var folderName = Path.GetFileName(subdir);
+
+            var destinationPath = Path.Combine(TempPath, folderName);
+
+            // Move the folder and keep track of the move
+            Directory.Move(subdir, destinationPath);
+            _movedFolders.Add((subdir, destinationPath));
+            Console.WriteLine($"Moved {folderName} to temp.");
+        }
+    }
+
+    public static void RollbackAction()
+    {
+        var movedFolders = _movedFolders;
+
+        foreach (var (sourcePath, destinationPath) in movedFolders)
+        {
+            // Move folders back to their original locations
+            Directory.Move(destinationPath, sourcePath);
+            Console.WriteLine($"Rolled back {Path.GetFileName(sourcePath)} to original location.");
+        }
+
+        movedFolders.Clear();
+
+        Directory.Delete(TempPath);
+
+        Console.WriteLine("Rollback completed.");
+    }
+}
---
+++ MudRunnerModsHelper/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace MudRunnerModsHelper;
+
+internal static class Settings
+{
+    public static string Config = "Config.txt";
+    public static int Timeout;
+
+    public static void SetMovingFoldersBackTimeout()
+    {
+        var time = File.ReadAllLines(Config).Last();
+
+        if (!int.TryParse(time, out Timeout))
+        {
+            Console.WriteLine("Couldn't parse timeout");
+
+            Environment.Exit(0);
+        }
+
+        Timeout *= 1000;
+    }
+}
